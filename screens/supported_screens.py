3132f921daaaf505548a0c4b4a93fdcf5957c53c
Trachou2Bois <trachou2bois@free.fr>
2025-09-23 20:15:53 +0200

    add fbname  to supported_screens for dtoverlay

--- screens/supported_screens.py
+++ screens/supported_screens.py
@@ -10,8 +10,8 @@ SCREEN_METADATA = {
     'SSD1306 0.96"':  {"resolution": "128x64 ",  "type": "i2c", "id": "SSD1306", "color": "MONO"},
     'SSD1315 0.96"':  {"resolution": "128x64 ",  "type": "i2c", "id": "SSD1306", "color": "MONO"},
     'SSD1309 2.49"':  {"resolution": "128x64 ",  "type": "i2c", "id": "SSD1306", "color": "MONO"},
-    'SSD1351 1.5" ':  {"resolution": "128x128",  "type": "spi", "id": "SSD1351", "color": "RGB", "speed": "8000000", "txbuflen": "65536"},
-    'ST7735 1.77" ':  {"resolution": "128x160",  "type": "spi", "id": "ST7735R", "color": "RGB", "speed": "14000000", "txbuflen": "65536"},
-    'ST7789 1.9"  ':  {"resolution": "170x320",  "type": "spi", "id": "ST7789W", "color": "RGB", "speed": "64000000", "txbuflen": "262144"},
-    'ST7789 2"    ':  {"resolution": "240x320",  "type": "spi", "id": "ST7789V", "color": "RGB", "speed": "64000000", "txbuflen": "262144"},
+    'SSD1351 1.5" ':  {"resolution": "128x128",  "type": "spi", "id": "SSD1351", "color": "RGB", "fbname": "ssd1351", "speed": "8000000", "txbuflen": "65536"},
+    'ST7735 1.77" ':  {"resolution": "128x160",  "type": "spi", "id": "ST7735R", "color": "RGB", "fbname": "st7735r", "speed": "14000000", "txbuflen": "65536"},
+    'ST7789 1.9"  ':  {"resolution": "170x320",  "type": "spi", "id": "ST7789W", "color": "RGB", "fbname": "st7789v", "speed": "64000000", "txbuflen": "262144"},
+    'ST7789 2"    ':  {"resolution": "240x320",  "type": "spi", "id": "ST7789V", "color": "RGB", "fbname": "st7789v", "speed": "64000000", "txbuflen": "262144"},
 }
