77aaa91a105495d2d5c4ce46ce0ba30a0019ec14
Trachou2Bois <trachou2bois@free.fr>
2025-12-18 10:15:55 +0100

    SSD1306, SSD1315 and SSD1309 SPI now supported

--- screens/supported_screens.py
+++ screens/supported_screens.py
@@ -4,7 +4,7 @@
 
 SCREEN_METADATA = {
     'SSD1306/09/15 I2C       ':  {"resolution": "128x64 ",  "type": "i2c", "id": "SSD1306", "color": "MONO"},
-    'SSD1306/09/15 SPI (to test)':  {"resolution": "128x64 ",  "type": "spi2c", "id": "SSD1306", "color": "MONO"},
+    'SSD1306/09/15 SPI       ':  {"resolution": "128x64 ",  "type": "spi2c", "id": "SSD1306", "color": "MONO"},
     'SSD1351 1.5" SPI        ':  {"resolution": "128x128",  "type": "spi", "id": "SSD1351", "color": "BGR", "fbname": "ssd1351", "speed": "8000000", "txbuflen": "65536"},
     'ST7735 1.77" SPI        ':  {"resolution": "128x160",  "type": "spi", "id": "ST7735R", "color": "RGB", "fbname": "st7735r", "speed": "14000000", "txbuflen": "65536"},
     'ST7789 1.9" SPI         ':  {"resolution": "170x320",  "type": "spi", "id": "ST7789W", "color": "RGB", "fbname": "st7789v", "speed": "64000000", "txbuflen": "262144"},
